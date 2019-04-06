76bf674be9ff4118dc2e423c91b8239bb5c50809
andrewbartels1 <46938273+andrewbartels1@users.noreply.github.com>
2019-04-06 11:51:18 -0700

    first commit

---
+++ tic_toc.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 23:43:07 2019
+
+@author: andre
+"""
+
+def tic():
+    #Homemade version of matlab tic and toc functions
+    import time
+    global startTime_for_tictoc
+    startTime_for_tictoc = time.time()
+
+def toc():
+    import time
+    if 'startTime_for_tictoc' in globals():
+        print("Elapsed time is " + str(time.time() - startTime_for_tictoc) + " seconds.")
+    else:
+        print("Toc: start time not set")
